9932d966765fbf4d222fa7da4f08adf191475780
lukedev45 <lukedevlin45@gmail.com>
2026-03-12 08:23:08 +0000

    Day 3 starting with what the value for EOF is

--- day2/input_output/file_copying.c
+++ day2/input_output/file_copying.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
-int main(void)
+/* int main(void)
 {
     int c;
-
+    printf("Input: ");
     c = getchar();
     while (c != EOF)
     {
         putchar(c);
         c = getchar();
+    }
+    return 0;
+}
+*/
+
+int main(void)
+{
+    int c;
+    while (c = getchar() != EOF)
+    {
+        printf("%d", c);    // This prints 1 once a character is input into the terminal
     }
     return 0;
 }
