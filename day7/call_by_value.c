1393cbff76ede7ba42846101e3c7c27bc21f194e
lukedev45 <lukedevlin45@gmail.com>
2026-03-16 23:04:34 +0000

    Starting char arrays

---
+++ day7/call_by_value.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#define MAXLINE 100
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+// Print the longest input line
+int main(void)
+{
+  int len;
+  int max;
+  char line[MAXLINE];
+  char longest[MAXLINE];
+  
+  max = 0;
+  while (line = getline(line, MAXLINE))
+  {
+    if (len > max)
+    {
+      max = len;
+      copy(longest,line);
+    }
+    if (max > 0)
+    {
+      printf("%s\n", longest);
+    }
+  }
+  return 0;
+}
+
+int getline(char s[], int lim)
+{
+  int c, i;
+
+  for (i = 0; i < lim - 1 && getchar() != EOF && c != '\n'; ++i)
+  {
+    s[i] = c;
+  }
+  if (c == '\n')
+  {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+void copy(char to[], char from[])
+{
+  int i;
+
+  i = 0;
+  while ((to[i] = from[i]) != '\0')
+  {
+    ++i;
+  }
+}
